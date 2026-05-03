90c8019e26d9b24436de4a848450938d0635fbdb
Stallone <38835624+Stallones@users.noreply.github.com>
2026-05-04 03:48:18 +0800

    Delete sta-ui-blog directory

--- sta-ui-blog/src/const/Visits/index.ts
+++
@@ -1,2 +0,0 @@
-// 文章访问量统计 前缀常量
-export const ARTICLE_VISIT_PREFIX = 'article_visit_'
--- sta-ui-blog/src/utils/base64-img/loading-img.ts
+++
@@ -1,2 +0,0 @@
-// export const loadingImg = "https://image.kuailemao.xyz/blog/loading/img_loading.gif"
-export const loadingImg = import.meta.env.VITE_MINIO_SERVE + '/blog/loading.gif';
